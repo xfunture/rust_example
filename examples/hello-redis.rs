c133961d9950bd654e76fcdbd82ecdf1f63669f9
xfunture <15768184216@163.com>
2024-01-05 15:44:52 +0800

    add 404.html,hello.html

---
+++ examples/hello-redis.rs
@@ -0,0 +1,25 @@
+
+// mini_redis
+use mini_redis::{client,Result};
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    // 建立与mini-redis服务器的连接
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // println!("client:{:#?}",client);
+    // 设置 key: "hello" 和 值: "world"
+    let result = client.set("hello", "world".into()).await?;
+    println!("result:{:?}",result);
+
+
+    // 获取"key=hello"的值
+    let result = client.get("hello").await?;
+
+    println!("从服务器端获取到结果={:?}", result);
+
+
+    Ok(())
+}
