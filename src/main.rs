5d12291395ba49679b27296f6691abf3ce6f1691
xfunture <15768184216@163.com>
2024-02-23 18:31:36 +0800

    测试broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use tokio::{self,runtime::Runtime,time::{self,Duration}};
-
-
+use tokio::sync::broadcast::{self,Sender};
+use tokio::task::JoinSet;
+use chrono::Local;
+use anyhow::Result;
 async fn sleep(n:u64) -> u64{
     time::sleep(Duration::from_secs(n)).await;
     n
 }
 
+fn now() -> String {
+    Local::now().format("%F %T").to_string()
+}
 
-#[tokio::main]
-async fn main(){
+
+
+// #[tokio::main]
+// async fn main(){
     // let rt = Runtime::new().unwrap();
     // rt.block_on(async{
 
@@ -19,15 +26,26 @@ async fn main(){
     //     println!("select! done");
     // });
 
-    let mut count = 0u8;
-    println!("count:{}",count);
-        tokio::select! {
-            // 如果取消biased，挑选的任务顺序将随机，可能会导致分支中的断言失败
-            // biased;
-            _ = async {println!("hello1,{}",count)}, if count < 1 => { count += 1; assert_eq!(count, 1); }
-            _ = async {println!("hello2,{}",count)}, if count < 2 => { count += 1; assert_eq!(count, 2); }
-            _ = async {println!("hello3,{}",count)}, if count < 3 => { count += 1; assert_eq!(count, 3); }
-            _ = async {println!("hello4,{}",count)}, if count < 4 => { count += 1; assert_eq!(count, 4); }
-            else => { println!("end"); }
-        };
+    //测试select!宏
+    // let mut count = 0u8;
+    // println!("count:{}",count);
+    // loop{
+    //     tokio::select! {
+    //         // 如果取消biased，挑选的任务顺序将随机，可能会导致分支中的断言失败
+    //         biased;q
+    //         _ = async {println!("hello1,{}",count)}, if count < 1 => { count += 1; assert_eq!(count, 1); }
+    //         _ = async {println!("hello2,{}",count)}, if count < 2 => { count += 1; assert_eq!(count, 2); }
+    //         _ = async {println!("hello3,{}",count)}, if count < 3 => { count += 1; assert_eq!(count, 3); }
+    //         _ = async {println!("hello4,{}",count)}, if count < 4 => { count += 1; assert_eq!(count, 4); }
+    //         else => { println!("end"); }
+    //     };
+    // }
+
+// }
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("hello,world");
+    Ok(())
 }
